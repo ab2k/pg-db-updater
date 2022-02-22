e538990f5ec2562fe3ae97951bef8df63903a322
Andrey Bolshakov <25472940+ab2k@users.noreply.github.com>
2022-02-22 03:26:18 +0300

    Update pg_db_updater.sh

--- pg_db_updater.sh
+++ pg_db_updater.sh
@@ -3,6 +3,19 @@
 # Copyright (C) 2022 Andrey Bolshakov
 # https://github.com/ab2k/pg-db-updater
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# maybe any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 set -o errexit
 if [ -f "./pg_db_updater_config.sh" ]; then
   source ./pg_db_updater_config.sh
